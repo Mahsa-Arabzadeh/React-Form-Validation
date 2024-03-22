dd519926cb3ec99b4564807ad41e51a5a8f4bb6e
Mahsa <arabzadehmah@gmail.com>
2024-03-22 14:08:21 +0430

    Refactor (submit form: add conditional rendering).

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -6,22 +6,19 @@ export default class Form extends Component {
     constructor(props) {
         super(props)
 
-        // this.submitForm = this.submitForm.bind(this)
         this.firstNameFunc = this.firstNameFunc.bind(this)
         this.lastNameFunc = this.lastNameFunc.bind(this)
         this.emailValid = this.emailValid.bind(this)
+        this.submitForm = this.submitForm.bind(this)
 
         this.state = {
             firstNameData: "",
             lastNameData: "",
             emailData: "",
-            allValid: true
+            allValid: true,
+            submited: false
         }
     }
-
-    // submitForm(){
-
-    // }
 
     firstNameFunc(e){
       this.setState({
@@ -41,15 +38,31 @@ export default class Form extends Component {
       })
     }
 
+    submitForm(e){
+      e.preventDefault()
+      this.setState({
+        submited: true
+      })
+    }
+
 
   render() {
     return (
       <div>
-        <form action="" id='form-valid'>
+        <form action="" id='form-valid' onSubmit={this.submitForm} autoComplete='off'>
         <div className="input-container">
           <input id='first-name' type="text" placeholder='First Name' value={this.state.firstNameData} onChange={this.firstNameFunc} />
-          <input id='last-name' type="text" placeholder='Last Name' value={this.state.lastNameData} onChange={this.lastNameFunc}/>
+          {this.state.submited && this.state.firstNameData === "" &&          
+                  <span id='error-handler'>Please enter an first name</span>
+          }
+                  <input id='last-name' type="text" placeholder='Last Name' value={this.state.lastNameData} onChange={this.lastNameFunc}/>
+          {this.state.submited && this.state.lastNameData === "" &&          
+                  <span id='error-handler'>Please enter an last name</span>
+          }
           <input id='email-data' type="email" placeholder='Email address' value={this.state.emailData} onChange={this.emailValid} />
+          {this.state.submited && this.state.emailData.length === 0 &&          
+                  <span id='error-handler'>Please enter an email address</span>
+          }
           <input type="submit" id='submit'/>
         </div>
       </form>

e3b33824259e4f5dd0e0c71cab949392e82c0e8b
Mahsa <arabzadehmah@gmail.com>
2024-08-06 19:54:33 +0430

    add

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Helmet } from "react-helmet"
+import Form from "./components/Form"
+
+function App() {
+
+  return (
+    <>
+      <Form/>
+      {/* for practice helmet package */}
+      <Helmet title="React Form"/>
+    </>
+  )
+}
+
+export default App
